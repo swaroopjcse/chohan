cfa59bab5b5fd1b11dc77d756dc3b038714de7c2
Swaroop Joshi <swaroopjoshi@ieee.org>
2023-03-29 00:59:17 +0530

    Clean up some code

--- app/src/main/java/yeb/dicegame/MainActivity.java
+++ app/src/main/java/yeb/dicegame/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
@@ -15,26 +16,28 @@ public class MainActivity extends AppCompatActivity {
     TextView txtD1, txtD2;
     TextView txtCoins;
     Button btnPlay;
-    Die d1, d2;
     private SwitchCompat switchOddEven;
     private EditText editWager;
+    Die d1, d2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnPlay = findViewById(R.id.btn_play);
-        txtD1 = findViewById(R.id.txt_d1);
-        txtD2 = findViewById(R.id.txt_d2);
-        txtCoins = findViewById(R.id.txt_coins);
+        getAllViewElements();
+        setNewDice();
+        updateUI(100);
 
-        switchOddEven = findViewById(R.id.switch_odd_even);
-        editWager = findViewById(R.id.edit_wager);
+        setPlayButtonClickListener();
+    }
 
+    private void setNewDice() {
         d1 = new Die();
         d2 = new Die();
+    }
 
+    private void setPlayButtonClickListener() {
         btnPlay.setOnClickListener(v -> {
             d1.roll();
             d2.roll();
@@ -44,6 +47,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void getAllViewElements() {
+        btnPlay = findViewById(R.id.btn_play);
+        txtD1 = findViewById(R.id.txt_d1);
+        txtD2 = findViewById(R.id.txt_d2);
+        txtCoins = findViewById(R.id.txt_coins);
+        switchOddEven = findViewById(R.id.switch_odd_even);
+        editWager = findViewById(R.id.edit_wager);
+    }
 
     private int calculateCoins(TextView txtCoins, EditText editWager, @NonNull SwitchCompat switchOddEven) {
         int coins = getIntFromTextView(txtCoins);
@@ -52,24 +63,33 @@ public class MainActivity extends AppCompatActivity {
 
         int diceSum = d1.value() + d2.value();
 
-        if (betForEven && isEven(diceSum)) {
-            coins += wager;
-        } else if (!betForEven && !isEven(diceSum)) {
+        boolean win = (betForEven && isEven(diceSum)) || (!betForEven && !isEven(diceSum));
+
+        if (win) {
+            showToast("Congratulations!");
             coins += wager;
         } else {
+            showToast("Aww! Try again!");
             coins -= wager;
         }
         return coins;
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     private boolean isEven(int num) {
         return num % 2 == 0;
     }
 
-    private void updateUI(int newCoins) {
+    private void updateUI(int numCoins) {
         showIntOnATextView(txtD1, d1.value());
         showIntOnATextView(txtD2, d2.value());
-        showIntOnATextView(txtCoins, newCoins);
+        showIntOnATextView(txtCoins, numCoins);
+
+        editWager.setText("");
+        switchOddEven.setChecked(false);
     }
 
     private void showIntOnATextView(@NonNull TextView txtView, int num) {
