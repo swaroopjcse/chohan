1652efcf3eb0dd8676ce4074b3a166609cab02d4
Swaroop Joshi <swaroopjoshi@ieee.org>
2023-03-29 00:09:59 +0530

    Grab elements

--- app/src/main/java/yeb/dicegame/MainActivity.java
+++ app/src/main/java/yeb/dicegame/MainActivity.java
@@ -3,12 +3,36 @@ package yeb.dicegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtD1, txtD2;
+    TextView txtCoins;
+    Button btnPlay;
+    int coins;
+    Die d1, d2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPlay = findViewById(R.id.btn_play);
+        txtD1 = findViewById(R.id.txt_d1);
+        txtD2 = findViewById(R.id.txt_d2);
+        txtCoins = findViewById(R.id.txt_coins);
+
+        btnPlay.setOnClickListener(v -> {
+            EditText editWager = findViewById(R.id.edit_wager);
+            int wager = getIntFromEditText(editWager);
+
+        });
+    }
+
+    private int getIntFromEditText(EditText editText) {
+        String inputString = editText.getText().toString();
+        return Integer.parseInt(inputString);
     }
 }
